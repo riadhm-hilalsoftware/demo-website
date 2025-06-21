d019424d2ca840566b750a4d49ea961bfe3ed90b
Riadh Mouania <riadhm.hilalsoftware@gmail.com>
2025-06-21 15:49:45 +0100

    add chat widget

---
+++ src/components/chat-demo.tsx
@@ -0,0 +1,52 @@
+"use client";
+import Script from "next/script";
+
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ChatWidget: any;
+  }
+}
+
+export default function ChatWidget() {
+  const initChat = () => {
+    if (typeof window !== "undefined" && window.ChatWidget) {
+      window.ChatWidget.renderChatComponent(
+        "chat-widget",
+        "9f8a5c5d-4e13-4122-b75d-d9e288b65e21",
+        "f368590f-ddc5-4040-9758-5744803f81bc"
+      );
+    }
+  };
+
+  return (
+    <>
+      <Script
+        src="https://chat.hilalsoftware.tools/output.css"
+        strategy="afterInteractive"
+      />
+      <Script
+        src="https://unpkg.com/react@18/umd/react.production.min.js"
+        strategy="afterInteractive"
+      />
+      <Script
+        src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"
+        strategy="afterInteractive"
+      />
+      <Script
+        src="https://chat.hilalsoftware.tools/chat_v2.js"
+        strategy="afterInteractive"
+        onLoad={initChat}
+      />
+      <div
+        id="chat-widget"
+        style={{
+          position: "fixed",
+          bottom: "1rem",
+          right: "1rem",
+          zIndex: 9999,
+        }}
+      />
+    </>
+  );
+}
